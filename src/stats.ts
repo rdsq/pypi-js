1f03446b1bed52938f3e858b33de663ea3d75ccc
rdsq <62446963+rdsq@users.noreply.github.com>
2024-08-12 15:07:40 +0300

    fmt

--- src/stats.ts
+++ src/stats.ts
@@ -5,8 +5,8 @@ export const statsEndpoint = 'https://pypi.org/stats/';
 export async function stats(): Promise<PypiStats> {
     const res = await fetch(statsEndpoint, {
         headers: {
-            Accept: "application/json",
-        }
+            Accept: 'application/json',
+        },
     });
     if (!res.ok) throw new Error(`${res.status}: ${res.statusText}`);
     return await res.json();
