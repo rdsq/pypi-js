b5f146e2dd39142f86ba5814a7a972b10fcf3601
rdsq <62446963+rdsq@users.noreply.github.com>
2024-08-12 15:02:27 +0300

    pasted stats type

---
+++ src/stats.d.ts
@@ -0,0 +1,15 @@
+export type PypiStats = {
+    "top_packages": {
+        "CodeIntel": {
+            "size": 23767329521
+        },
+        "Fiona": {
+            "size": 6209535709
+        },
+        "FlexGet": {
+            "size": 4387002448
+        },
+        "...": "..."
+    },
+    "total_packages_size": 23965450269
+};
