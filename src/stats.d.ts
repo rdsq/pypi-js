82130f3bbc0dc8585ceee2a8a08d02fad11f3feb
rdsq <62446963+rdsq@users.noreply.github.com>
2024-08-12 15:28:26 +0300

    stats type docs

--- src/stats.d.ts
+++ src/stats.d.ts
@@ -2,10 +2,12 @@
  * Stats of the PyPI registry. [Reference](https://warehouse.pypa.io/api-reference/stats.html)
  */
 export type PypiStats = {
+    /** Top 100 packages + their size in bytes */
     "top_packages": {
         [key: string]: {
             "size": number
         },
     },
+    /** Total size of PyPI packages in bytes */
     "total_packages_size": number
 };
