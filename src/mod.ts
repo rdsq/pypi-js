5f4cb46e90999df74c03fbbdf94c9f13f17ca537
rdsq <62446963+rdsq@users.noreply.github.com>
2024-08-12 15:07:28 +0300

    stats

--- src/mod.ts
+++ src/mod.ts
@@ -2,5 +2,6 @@ import { all, iterate } from './all.ts';
 import { getPackage } from './get.ts';
 import type { PypiPackage } from './package.d.ts';
 import type { PypiStats } from './stats.d.ts';
+import { stats } from './stats.ts';
 
-export { all, getPackage, iterate, type PypiPackage, type PypiStats };
+export { all, getPackage, iterate, type PypiPackage, type PypiStats, stats };
---
+++ src/stats.ts
@@ -0,0 +1,13 @@
+import type { PypiStats } from './stats.d.ts';
+
+export const statsEndpoint = 'https://pypi.org/stats/';
+
+export async function stats(): Promise<PypiStats> {
+    const res = await fetch(statsEndpoint, {
+        headers: {
+            Accept: "application/json",
+        }
+    });
+    if (!res.ok) throw new Error(`${res.status}: ${res.statusText}`);
+    return await res.json();
+}
